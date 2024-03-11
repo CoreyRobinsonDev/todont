522f56aca9dc44f5adf308ab9f8c9118430c3379
CoreyRobinsonDev <coreyrob52@gmail.com>
2024-03-11 02:34:19 -0400

    feat: add put on notes/:id endpoint

--- src/api/note.rs
+++ src/api/note.rs
@@ -1,8 +1,10 @@
+use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::{extract::State, Json, response::IntoResponse};
 use serde::Deserialize;
 use serde_json::json;
 use sqlx::prelude::FromRow;
+use sqlx::types::chrono;
 use tower_cookies::Cookies;
 
 use crate::TodontDB;
@@ -23,7 +25,7 @@ pub async fn create_note(
         return Err(Error::Auth(Auth::Session));
     };
 
-    let Ok(row) = sqlx::query_as::<_, NoteId>("
+    let Ok(id) = sqlx::query_as::<_, NoteId>("
         INSERT INTO note
         (user_id, title, description, completed)
         VALUES ($1, $2, $3, $4)
@@ -39,7 +41,7 @@ pub async fn create_note(
 
     return Ok((StatusCode::CREATED, Json(json!({
         "success": true,
-        "message": row.id.to_string()
+        "message": id.0.to_string()
     }))))
 }
 
@@ -68,14 +70,49 @@ pub async fn get_notes(
     }))))
 }
 
-#[derive(Debug, FromRow)]
-struct NoteId {
-    id: i32
+pub async fn update_note(
+    cookies: Cookies,
+    State(state): State<TodontDB>,
+    Path(id): Path<i32>,
+    payload: Json<NotePayload> 
+) -> Result<impl IntoResponse> {
+    println!("->> {:<12} - update_note", "HANDLER");
+
+    let Some(user) = get_user(&cookies, &state.pool).await else {
+        return Err(Error::Auth(Auth::Session));
+    };
+
+    let Ok(_) = sqlx::query("
+        UPDATE note
+        SET title = $1,
+        description = $2,
+        updated_at = $3
+        WHERE user_id = $4
+        AND id = $5
+        RETURNING id")
+        .bind(&payload.title)
+        .bind(&payload.description)
+        .bind(chrono::Utc::now())
+        .bind(&user.id)
+        .bind(&id)
+        .fetch_one(&state.pool)
+        .await else {
+            return Err(Error::Client);
+        };
+
+    return Ok((StatusCode::CREATED, Json(json!({
+        "success": true,
+        "message": id.to_string()
+    }))))
+
 }
+
+#[derive(Debug, FromRow, Deserialize)]
+struct NoteId(i32);
 
 #[derive(Debug, Deserialize)]
 pub struct NotePayload {
-     title: String,
-     description: Option<String>,
-     completed: Option<bool>,
+    title: String,
+    description: Option<String>,
+    completed: Option<bool>,
 }
